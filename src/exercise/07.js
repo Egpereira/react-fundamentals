e946f7436acdcab44ce65625c7dfa4530261e7db
Eduardo Pereira <egpereira05@gmail.com>
2023-10-23 12:06:40 -0300

    Add exercise 7 solution

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -4,10 +4,10 @@
 import * as React from 'react'
 
 const allItems = [
-  {id: 'apple', value: '🍎 apple'},
-  {id: 'orange', value: '🍊 orange'},
-  {id: 'grape', value: '🍇 grape'},
-  {id: 'pear', value: '🍐 pear'},
+  { id: 'apple', value: '🍎 apple' },
+  { id: 'orange', value: '🍊 orange' },
+  { id: 'grape', value: '🍇 grape' },
+  { id: 'pear', value: '🍐 pear' },
 ]
 
 function App() {
@@ -23,14 +23,13 @@ function App() {
   }
 
   return (
-    <div className="keys">
+    <div className='keys'>
       <button disabled={items.length >= allItems.length} onClick={addItem}>
         add item
       </button>
       <ul>
         {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
